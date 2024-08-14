bb87a8c331218cf9a777610c175f442069116670
shailyy <166197783+ShailyBhargava@users.noreply.github.com>
2024-08-14 21:30:16 +0530

    mean variance and std .py

--- mean variance and std .py
+++ mean variance and std .py
@@ -9,10 +9,10 @@ def calculate(input):
     cal =dict()
     cal['mean'] =[a.mean(0).tolist(),a.mean(1).tolist(),a.mean().tolist()]
     cal['standard deviation'] =[a.std(0).tolist(),a.std(1).tolist(),a.std().tolist()]
-    cal['max'] =a.max(0).tolist(),a.max(1).tolist(),a.max().tolist()
-    cal['min'] =a.min(0).tolist(),a.min(1).tolist(),a.min().tolist()
-    cal['variance'] =a.var(0).tolist(),a.var(1).tolist(),a.var().tolist()
-    cal['sum'] =a.sum(0).tolist(),a.sum(1).tolist(),a.sum().tolist()
+    cal['max'] =[a.max(0).tolist(),a.max(1).tolist(),a.max().tolist()]
+    cal['min'] =[a.min(0).tolist(),a.min(1).tolist(),a.min().tolist()]
+    cal['variance'] =[a.var(0).tolist(),a.var(1).tolist(),a.var().tolist()]
+    cal['sum'] =[a.sum(0).tolist(),a.sum(1).tolist(),a.sum().tolist()]
     return cal
 print(calculate([0,1,2,3,4,5,6,7,8]))
 
